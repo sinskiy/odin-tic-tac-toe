f1e5db6d9259fd3ddf175238be8b8eb5ea0f2380
sinskiy <sinskiy@proton.me>
2024-06-18 18:00:23 +0300

    feat: add possibility to play rounds

--- script.js
+++ script.js
@@ -2,6 +2,45 @@ const Gameboard = (function () {
   const CELLS = 9;
 
   const board = Array(CELLS).fill(null);
+
+  let players = [];
+  let active = 0;
+
+  const playGame = (player1, player2) => {
+    players = [player1, player2];
+    while (true) {
+      playRound();
+    }
+  };
+
+  const playRound = () => {
+    let choice = -1;
+    while (isInvalidChoice()) {
+      choice = Number(prompt("choice")) - 1;
+    }
+
+    markCell(players[active].mark, choice);
+    switchPlayerTurn();
+
+    function isInvalidChoice() {
+      return isNaN(choice) || choice < 0 || choice > 8;
+    }
+
+    function markCell(playerMark, cellPosition) {
+      board[cellPosition] = playerMark;
+    }
+    function switchPlayerTurn() {
+      const lastPlayerIndex = players.length - 1;
+      if (active === lastPlayerIndex) {
+        active = 0;
+      } else {
+        active++;
+      }
+    }
+    console.log(board);
+  };
+
+  return { playGame };
 })();
 
 function createPlayer(name, mark) {
@@ -10,3 +49,4 @@ function createPlayer(name, mark) {
 
 const player1 = createPlayer("sinskiy", "x");
 const player2 = createPlayer("kilwinta", "o");
+Gameboard.playGame(player1, player2);
