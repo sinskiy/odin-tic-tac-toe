09e2c1f1f70428ac14e4d392a23c97e8cfd9bfb9
sinskiy <sinskiy@proton.me>
2024-06-18 18:02:45 +0300

    feat: determine winner

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const Gameboard = (function () {
 
   const playGame = (player1, player2) => {
     players = [player1, player2];
-    while (true) {
+    while (getWinner() === null) {
       playRound();
     }
   };
@@ -37,8 +37,27 @@ const Gameboard = (function () {
         active++;
       }
     }
-    console.log(board);
   };
+
+  function getWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    return null;
+  }
 
   return { playGame };
 })();
